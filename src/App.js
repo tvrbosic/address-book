d6d774ec8311966670c622145ece73f0877f1480
tvrbosic <tomislav.vrbosic@gmail.com>
2022-09-27 14:47:38 +0200

    If user is not logged in redirect to login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import LoginPage from './pages/LoginPage';
 import ContactsPage from './pages/ContactsPage';
 
 function App() {
+  const userLoggedIn = useSelector((state) => state.user.loggedIn);
   return (
     <Routes>
-      <Route exact path='/' element={<LoginPage />}></Route>
-      <Route exact path='/contacts' element={<ContactsPage />}></Route>
+      {!userLoggedIn && <Route exact path='/' element={<LoginPage />} />}
+      {userLoggedIn && (
+        <Route exact path='/contacts' element={<ContactsPage />} />
+      )}
+      <Route path='*' element={<Navigate to='/' replace />} />
     </Routes>
   );
 }
