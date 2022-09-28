781238f4e1e3b534f9a5fb3bdca69c3f16edaa29
tvrbosic <tomislav.vrbosic@gmail.com>
2022-09-28 19:48:14 +0200

    Main panel and search panel styling in progress

--- src/pages/contacts/MainPanel.js
+++ src/pages/contacts/MainPanel.js
@@ -1,5 +1,15 @@
+import { Row, Col, Button } from 'react-bootstrap';
+
 const MainPanel = () => {
-  return <div>MainPanel</div>;
+  return (
+    <Row className='d-flex align-items-center px-5 py-3'>
+      <Col className='fs-5 fw-bold'>My Contacts</Col>
+      <Col>Dropdown</Col>
+      <Col>
+        <Button variant='outline-success'>Add Contact</Button>
+      </Col>
+    </Row>
+  );
 };
 
 export default MainPanel;
--- src/pages/contacts/SearchPanel.js
+++ src/pages/contacts/SearchPanel.js
@@ -1,5 +1,37 @@
+import { Row, Col, Form, InputGroup, NavDropdown } from 'react-bootstrap';
+import { Search } from 'react-bootstrap-icons';
+
 const SearchPanel = () => {
-  return <div>SearchPanel</div>;
+  return (
+    <Row className='d-flex align-items-center px-5 py-4 border-top bg-light'>
+      <Col>
+        <InputGroup>
+          <InputGroup.Text id='search-input'>
+            <Search className='fs-5' />
+          </InputGroup.Text>
+          <Form.Control
+            placeholder='Search...'
+            aria-label='Search'
+            aria-describedby='search-input'
+          />
+        </InputGroup>
+      </Col>
+
+      <Col>
+        <InputGroup>
+          <input type='date' />
+        </InputGroup>
+      </Col>
+      <Col>
+        <NavDropdown id='type-dropdown' title='Type' menuVariant='light'>
+          <NavDropdown.Item>Mobile</NavDropdown.Item>
+          <NavDropdown.Item>Landline</NavDropdown.Item>
+          <NavDropdown.Item>Email</NavDropdown.Item>
+          <NavDropdown.Item>Pager</NavDropdown.Item>
+        </NavDropdown>
+      </Col>
+    </Row>
+  );
 };
 
 export default SearchPanel;
