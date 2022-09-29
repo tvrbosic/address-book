1e4d5f2164d29da2fdbb73f79dd22acff66578e1
tvrbosic <tomislav.vrbosic@gmail.com>
2022-09-29 22:47:11 +0200

    TableHeader component, hover pointer on header

--- src/components/table/DataTable.js
+++ src/components/table/DataTable.js
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 
 import { Table } from 'react-bootstrap';
 
-import SortedIcon from './SortedIcon';
+import TableHeader from './TableHeader';
 import TableRow from './TableRow';
 import PaginationControl from '../PaginationControl';
 
@@ -11,8 +11,10 @@ const itemsPerPage = 20;
 
 const DataTable = (props) => {
   const [currentPage, setCurrentPage] = useState(1);
-  const [sortedBy, setSortedBy] = useState(null);
-  const sortAscending = useSelector((state) => state.contacts.sortAscending);
+  const [sortBy, setSortBy] = useState(null);
+  const attributeSortDirections = useSelector(
+    (state) => state.contacts.sortAscending
+  );
   const maxPage = Math.ceil(props.data.length / itemsPerPage);
 
   const currentPageData = props.data.slice(
@@ -20,59 +22,19 @@ const DataTable = (props) => {
     currentPage * itemsPerPage
   );
 
-  const sortByHandler = (sortAttribute) => {
-    setSortedBy(sortAttribute);
+  const sortHandler = (sortAttribute) => {
+    setSortBy(sortAttribute);
     props.sort(sortAttribute);
   };
 
   return (
     <>
       <Table hover className='text-center align-middle border-top'>
-        <thead className='align-middle'>
-          <tr>
-            <th onClick={() => sortByHandler('name')}>
-              <span>Name</span>
-              <SortedIcon
-                visible={sortedBy === 'name'}
-                sortAscending={sortAscending['name']}
-                className='fs-4 ms-2'
-              />
-            </th>
-            <th onClick={() => sortByHandler('surname')}>
-              <span>Surname</span>
-              <SortedIcon
-                visible={sortedBy === 'surname'}
-                sortAscending={sortAscending['surname']}
-                className='fs-4 ms-2'
-              />
-            </th>
-            <th onClick={() => sortByHandler('birth')}>
-              <span>Date of birth</span>
-              <SortedIcon
-                visible={sortedBy === 'birth'}
-                sortAscending={sortAscending['birth']}
-                className='fs-4 ms-2'
-              />
-            </th>
-            <th onClick={() => sortByHandler('type')}>
-              <span>Contact type</span>
-              <SortedIcon
-                visible={sortedBy === 'type'}
-                sortAscending={sortAscending['type']}
-                className='fs-4 ms-2'
-              />
-            </th>
-            <th onClick={() => sortByHandler('contact')}>
-              <span>Contact</span>
-              <SortedIcon
-                visible={sortedBy === 'contact'}
-                sortAscending={sortAscending['contact']}
-                className='fs-4 ms-2'
-              />
-            </th>
-            <th></th>
-          </tr>
-        </thead>
+        <TableHeader
+          sortBy={sortBy}
+          attributeSortDirections={attributeSortDirections}
+          sortHandler={sortHandler}
+        />
 
         <tbody>
           {currentPageData.map((contact) => (
---
+++ src/components/table/TableHeader.js
@@ -0,0 +1,54 @@
+import styles from './TableHeader.module.css';
+import SortedIcon from './SortedIcon';
+
+const TableHeader = ({ sortBy, attributeSortDirections, sortHandler }) => {
+  return (
+    <thead className={`${styles.container} align-middle`}>
+      <tr>
+        <th onClick={() => sortHandler('name')}>
+          <span>Name</span>
+          <SortedIcon
+            visible={sortBy === 'name'}
+            sortAscending={attributeSortDirections['name']}
+            className='fs-4 ms-2'
+          />
+        </th>
+        <th onClick={() => sortHandler('surname')}>
+          <span>Surname</span>
+          <SortedIcon
+            visible={sortBy === 'surname'}
+            sortAscending={attributeSortDirections['surname']}
+            className='fs-4 ms-2'
+          />
+        </th>
+        <th onClick={() => sortHandler('birth')}>
+          <span>Date of birth</span>
+          <SortedIcon
+            visible={sortBy === 'birth'}
+            sortAscending={attributeSortDirections['birth']}
+            className='fs-4 ms-2'
+          />
+        </th>
+        <th onClick={() => sortHandler('type')}>
+          <span>Contact type</span>
+          <SortedIcon
+            visible={sortBy === 'type'}
+            sortAscending={attributeSortDirections['type']}
+            className='fs-4 ms-2'
+          />
+        </th>
+        <th onClick={() => sortHandler('contact')}>
+          <span>Contact</span>
+          <SortedIcon
+            visible={sortBy === 'contact'}
+            sortAscending={attributeSortDirections['contact']}
+            className='fs-4 ms-2'
+          />
+        </th>
+        <th></th>
+      </tr>
+    </thead>
+  );
+};
+
+export default TableHeader;
