b7cfc3df48960497da237bc1d9fb46668305f246
tvrbosic <tomislav.vrbosic@gmail.com>
2022-09-29 13:17:56 +0200

    contactsSlice addOrUpdate and removeContact actions

--- src/store/contacts-slice.js
+++ src/store/contacts-slice.js
@@ -11,14 +11,41 @@ const contactsSlice = createSlice({
     setContacts(state, action) {
       state.list = action.payload;
     },
-    addContact(state) {
-      // TODO:
+    addOrUpdateContact(state, action) {
+      const newContact = action.payload;
+      const existingContact = state.list.find(
+        (contact) => contact.id === newContact.id
+      );
+      if (!existingContact) {
+        // Contact does not exist
+        state.list.push({
+          id: newContact.id,
+          name: newContact.title,
+          surname: newContact.price,
+          birth: newContact.birth,
+          type: newContact.type,
+          contact: newContact.contact,
+          favourite: newContact.favourite,
+          star: newContact.star,
+        });
+      } else {
+        // Contact exists
+        existingContact.id = newContact.id;
+        existingContact.name = newContact.title;
+        existingContact.surname = newContact.price;
+        existingContact.birth = newContact.birth;
+        existingContact.type = newContact.type;
+        existingContact.contact = newContact.contact;
+        existingContact.favourite = newContact.favourite;
+        existingContact.star = newContact.star;
+      }
     },
-    updateContact(state) {
-      // TODO:
-    },
-    deleteContact(state) {
-      // TODO:
+    deleteContact(state, action) {
+      const id = action.payload;
+      const existingContact = state.list.find((contact) => contact.id === id);
+      if (existingContact) {
+        state.list = state.list.filter((contact) => contact.id !== id);
+      }
     },
   },
 });
